ff72ac5e73f6e72db3a9fbcc5c10ac03f10731b5
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2022-02-26 00:11:11 +0530

    ejs and route

---
+++ src/models/Bankdetails.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const bankdetails = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+    },
+    accountNumber: {
+        type: String,
+        trim: true,
+    },
+    mobileNumber: {
+        type: String,
+        trim: true,
+    },
+    ifscCode: {
+        type: String,
+        trim: true,
+    },
+    branch:{
+        type: String,
+        trim : true
+    },
+    city : {
+        type: String,
+        trim : true,
+        uppercase: true
+    },
+    state : {
+        type : String,
+        trim : true,
+        uppercase: true
+    }
+})
+
+const Contact = mongoose.model('Bankdetails', bankdetails)
+
+module.exports = bankdetails
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const Contact = require('../models/Contact');
+const Bankdetails = require('../models/Bankdetails');
 const { contactMail } = require('../config/nodemailer');
 //Route for homepage
 router.get('/', (req, res) => {
-    res.send('Home Page')
+    res.render('./index')
 });
 
 router.post('/contact',async(req,res)=>{
@@ -36,5 +37,34 @@ router.post('/contact',async(req,res)=>{
         return res.redirect('/');
     }
 })
+
+router.post('bankdetails', async(req,res)=>{
+      const { name,accountNumber,mobileNumber,ifscCode,branch,city,state} = req.body;
+      try{
+        const newBankdetails = await new Bankdetails({
+            name,
+            accountNumber,
+            mobileNumber,
+            ifscCode,
+            branch,
+            city,
+            state
+        }).save();
+        if(!newBankdetails){
+            req.flash('error_msg','Issue cannot be created');
+            return res.redirect('/');
+        }
+  
+        }
+        catch(err){
+            console.error(err);
+            return res.redirect('/');
+        }
+        console.log(req.body);
+        res.status(201).send('Bank details added successfully');
+});
+router.get('/bankdetails',(req,res)=>{
+    console.log(req.body);
+});
 
 module.exports = router
