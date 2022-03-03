1f9ba1f9e1c2cc6c13227b1e14a746e887bc4103
Debojyoti1915001 <debojyoti76@gmail.com>
2022-03-03 19:03:13 +0530

    added chat app

--- src/app.js
+++ src/app.js
@@ -17,65 +17,7 @@ require('dotenv').config()
 //Configuring Port
 const PORT = process.env.PORT || 3000
 
-const http = require("http");
-const socketio = require("socket.io");
-const Filter = require("bad-words");
-const { generateMessage, generateLocationMessage } = require("./utils/messages");
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./utils/chatuser.js");
-const server = http.createServer(app);
-const io = socketio(server);
 
-io.on("connection", socket => {
-  console.log("New WebSocket connection");
-
-  socket.on("join", (options, callback) => {
-    const { error, user } = addUser({ id: socket.id, ...options });
-    if (error) {
-      return callback(error);
-    } else {
-      socket.join(user.room);
-
-      socket.emit("message", generateMessage("Admin", "Welcome!"));
-      socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} has joined!`));
-      io.to(user.room).emit("roomData", {
-        room: user.room,
-        users: getUsersInRoom(user.room)
-      });
-
-      callback();
-    }
-  });
-
-  socket.on("sendMessage", (message, callback) => {
-    const user = getUser(socket.id);
-    const filter = new Filter();
-
-    if (filter.isProfane(message)) {
-      return callback("Profanity is not allowed!");
-    } else {
-      io.to(user.room).emit("message", generateMessage(user.username, message));
-      callback();
-    }
-  });
-
-  socket.on("sendLocation", (coords, callback) => {
-    const user = getUser(socket.id);
-    io.to(user.room).emit("locationMessage", generateLocationMessage(user.username, `https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`));
-    callback();
-  });
-
-  socket.on("disconnect", () => {
-    const user = removeUser(socket.id);
-
-    if (user) {
-      io.to(user.room).emit("message", generateMessage("Admin", `${user.username} has left!`));
-      io.to(user.room).emit("roomData", {
-        room: user.room,
-        users: getUsersInRoom(user.room)
-      });
-    }
-  });
-});
 //Mongoose connection
 mongoose
     .connect(process.env.MONGODB_URL, {
@@ -87,7 +29,7 @@ mongoose
     .then(() => console.log('Connected to mongo server'))
     .catch((err) => console.error(err))
 
-const publicDirectory = path.join(__dirname, '../public')
+const publicDirectory = path.join(__dirname, '../public/bharat')
     // console.log(publicDirectory);
 app.use(express.static(publicDirectory))
 
